89bc84d81aae173f5a68ec09cff7f7a678cd899f
Jamal Watts <jwatts721@yahoo.com>
2020-10-05 23:06:10 -0400

    Completed test project for CI Testing

---
+++ Python-Calculator-Test-Project/_tests/test_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from lib.calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        calc = Calculator()
+        self.assertEqual(calc.add(3, 4), 7, "Expected 7")
+
+    def test_subtract(self):
+        calc = Calculator()
+        self.assertEqual(calc.subtract(10, 7), 3, "Expected 3")
+
+    def test_multiply(self):
+        calc = Calculator()
+        self.assertEqual(calc.multiply(3, 4), 12, "Expected 12")
+
+    def test_divide(self):
+        calc = Calculator()
+        self.assertEqual(calc.divide(12, 6), 2, "Expected 2")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Python-Calculator-Test-Project/main.py
@@ -0,0 +1,9 @@
+from lib.calculator import Calculator
+
+calc = Calculator()
+
+x = 3
+y = 5
+
+z = calc.add(x, y)
+print(x, '+', y, ' = ', z)
