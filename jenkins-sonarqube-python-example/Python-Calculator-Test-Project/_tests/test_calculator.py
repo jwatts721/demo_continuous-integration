4227262a9ddd195b6f646c72a935aede4fcb54a5
Jamal Watts <jwatts721@yahoo.com>
2022-04-21 00:33:50 +0000

    test_calculator.py edited online with Bitbucket

--- jenkins-sonarqube-python-example/Python-Calculator-Test-Project/_tests/test_calculator.py
+++ jenkins-sonarqube-python-example/Python-Calculator-Test-Project/_tests/test_calculator.py
@@ -6,6 +6,7 @@ from lib.calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
 
+
     def test_add(self):
         calc = Calculator()
         self.assertEqual(calc.add(3, 4), 7, "Expected 7")
