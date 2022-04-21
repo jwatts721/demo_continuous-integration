0030c7d2c0c991cece2a3f2315d2e871f72758f6
Jamal Watts <jwatts721@yahoo.com>
2022-04-21 00:28:27 +0000

    calculator.py edited online with Bitbucket

--- jenkins-sonarqube-python-example/Python-Calculator-Test-Project/lib/calculator.py
+++ jenkins-sonarqube-python-example/Python-Calculator-Test-Project/lib/calculator.py
@@ -3,16 +3,13 @@ class Calculator:
     def add(x, y):
         return x + y
 
-
     @staticmethod
     def subtract(x, y):
         return x - y
 
-
     @staticmethod
     def multiply(x, y):
         return x * y
-
 
     @staticmethod
     def divide(x, y):
