a71b43211c9907943ffe28bfd3b64f97593a4563
Jamal Watts <jwatts721@yahoo.com>
2022-04-21 00:24:36 +0000

    test_calculator.py edited online with Bitbucket

--- jenkins-sonarqube-python-example/Python-Calculator-Test-Project/_tests/test_calculator.py
+++ jenkins-sonarqube-python-example/Python-Calculator-Test-Project/_tests/test_calculator.py
@@ -8,18 +8,22 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
         self.assertEqual(calc.add(3, 4), 7, "Expected 7")
+        
 
     def test_subtract(self):
         calc = Calculator()
         self.assertEqual(calc.subtract(10, 7), 3, "Expected 3")
+        
 
     def test_multiply(self):
         calc = Calculator()
         self.assertEqual(calc.multiply(3, 4), 12, "Expected 12")
+        
 
     def test_divide(self):
         calc = Calculator()
         self.assertEqual(calc.divide(12, 6), 2, "Expected 2")
+        
 
 if __name__ == '__main__':
     unittest.main()
